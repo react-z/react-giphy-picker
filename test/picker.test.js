1732dfc5786cbf43b9609c9b630729ea67a28a15
Arjun Komath <arjunkomath@gmail.com>
2018-06-14 00:55:08 +0400

    Fix test

--- test/picker.test.js
+++ test/picker.test.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Picker from '../src/Picker'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Giphy Picker component', () => {
 
-	it('should render GIF picker with onSelected prop', function (t) {
+	it('should render GIF picker with onSelected prop', async () => {
 		const log = (gif) => {
 			console.log(gif)
 		}
 
 		const wrapper = mount(<Picker onSelected={log} />)
-		expect(wrapper.props().onSelected).toEqual(log)
-		t()
+		await expect(wrapper.props().onSelected).toEqual(log)
 	})
 
-	it('should render GIF picker with custom api key', function (t) {
+	it('should render GIF picker with custom api key', async () => {
 		const log = (gif) => {
 			console.log(gif)
 		}
 		const apiKey = 'testing'
 
 		const wrapper = mount(<Picker onSelected={log} apiKey={apiKey} />)
-		expect(wrapper.props().apiKey).toEqual(apiKey)
-		expect(wrapper.state().giphySearchUrl).toEqual(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`)
-		expect(wrapper.state().giphyTrendingUrl).toEqual(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`)
-		t()
+		await expect(wrapper.props().apiKey).toEqual(apiKey)
+		await expect(wrapper.state().giphySearchUrl).toEqual(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`)
+		await expect(wrapper.state().giphyTrendingUrl).toEqual(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`)
 	})
 
 })
